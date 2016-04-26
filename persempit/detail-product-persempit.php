c9f083e47b35ca3d2b692b7f670f473bea25e54f
edisetiawan <edi2992@gmail.com>
2016-04-26 18:59:10 +0700

    versi 1.0

---
+++ persempit/detail-product-persempit.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>New Fashions a Flat Ecommerce Bootstrap Responsive Website Template | Single :: w3layouts</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="New Fashions Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" 
+		/>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" /> 
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" /> 
+<script src="js/jquery.min.js"></script>
+<link href="css/megamenu.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="js/megamenu.js"></script>  
+<script>$(document).ready(function(){$(".megamenu").megamenu();});</script>
+
+</head>
+<body>
+
+<div class="product-main">
+	 <div class="container">
+		 <ol class="breadcrumb">
+		  <li><a href="index.html">Home</a></li>
+		  <li class="active">Single</li>
+		 </ol>
+		 <div class="ctnt-bar cntnt">
+			 <div class="content-bar">
+				 <div class="single-page">					 
+						
+					 <div class="details-left-slider">
+						<div class="flexslider">
+							       
+										<script src="js/imagezoom.js"></script>
+										  <script defer src="js/jquery.flexslider.js"></script>
+										<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+
+										<script>
+										// Can also be used with $(document).ready()
+										$(window).load(function() {
+										  $('.flexslider').flexslider({
+											animation: "slide",
+											controlNav: "thumbnails"
+										  });
+										});
+										</script>
+									
+
+							  <ul class="slides">
+								<li data-thumb="persempit/images/s1.jpg">
+									<div class="thumb-image"> <img src="persempit/images/s1.jpg" data-imagezoom="true" class="img-responsive"> </div>
+								</li>
+								<li data-thumb="persempit/images/s2.jpg">
+									 <div class="thumb-image"> <img src="persempit/images/s2.jpg" data-imagezoom="true" class="img-responsive"> </div>
+								</li>
+								<li data-thumb="persempit/images/s3.jpg">
+								   <div class="thumb-image"> <img src="persempit/images/s3.jpg" data-imagezoom="true" class="img-responsive"> </div>
+								</li>
+								<li data-thumb="persempit/images/s4.jpg">
+								   <div class="thumb-image"> <img src="persempit/images/s4.jpg" data-imagezoom="true" class="img-responsive"> </div>
+								</li>
+							  </ul>
+							<div class="clearfix"></div>
+						</div>
+		
+					 </div>
+					 <div class="details-left-info">
+							<h3>Belle Fille Grey & Black</h3>
+								<h4>Pellentesque pretium </h4>							
+							<p>$ 180 <a href="#">Click for offer</a></p>
+							<p class="qty">Qty ::</p><input min="1" type="number" id="quantity" name="quantity" value="1" class="form-control input-small">
+							<div class="btn_form">
+								<a href="cart.html">Add to cart</a>
+							</div>
+							<div class="flower-type">
+							<p>Mobel  ::<a href="#">Dress</a></p>
+							<p>Brand  ::<a href="#">Pellentesque</a></p>
+							</div>
+							<h5>Description  ::</h5>
+							<p class="desc">Proin vestibulum scelerisque tempus. Phasellus at fermentum erat. Pellentesque mattis velit eget elit condimentum gravida. Donec vehicula mollis velit, a eleifend est hendrerit quis. Etiam pulvinar at ex eget cursus. Etiam luctus orci ut tortor rhoncus, sit amet faucibus lorem finibus. Nulla id ornare arcu. or lowest node and working upwards) are as follows:</p>
+					 </div>
+					 <div class="clearfix"></div>				 	
+				 </div>
+			 </div>
+		 </div>	
+
+
+
+	 </div>
+</div>
+
+
+
+</body>
+</html>
---
+++ template-ci.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Product Details | E-Shopper</title>
+
+    <!-- code persempit start-->
+	<!--<link href="persempit/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" /> -->
+	
+	<link href="persempit/css/style.css" rel="stylesheet" type="text/css" media="all" /> 
+	<script src="persempit/js/jquery.min.js"></script>
+	<link href="persempit/css/megamenu.css" rel="stylesheet" type="text/css" media="all" />  
+	<script type="text/javascript" src="persempit/js/megamenu.js"></script>  
+	<script>$(document).ready(function(){$(".megamenu").megamenu();});</script> 
+	<!-- code persempit end -->
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/price-range.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">  
+	
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+</head>
+<body>
+	<?php
+	include('header.php');
+	//include('slider.php');
+	//include('home1.php');
+	include('product-detail-ci.php');
+	//include('product-details.php');
+	//include('login.php');
+	//include('produk.php');
+	//include('cart.php');
+	//include('checkout.php');
+	//include('contact-us.php');
+	include('footer.php');
+	?>
+    <!--<script src="js/jquery.js"></script>  -->
+	<script src="js/price-range.js"></script>
+    <script src="js/jquery.scrollUp.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/main.js"></script> 
+
+</body>
+</html>
